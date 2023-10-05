a70f92cb204e1661b5631acb58049c78500762bc
noobster3 <144312595+noobster3@users.noreply.github.com>
2023-10-05 21:07:20 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,33 +1,39 @@
-import { Client } from 'node-appwrite';
+import { getStaticFile, throwIfMissing } from './utils.js';
+import twilio from 'twilio'
 
-// This is your Appwrite function
-// It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+export default async (context) => {
+  const { req, res, log, error } = context;
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+  throwIfMissing(process.env, [
+    'TWILIO_ACCOUNT_SID',
+    'TWILIO_AUTH_TOKEN',
+  ]);
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-  // The `req` object contains the request data
   if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+    return res.send(getStaticFile('index.html'), 200, {
+      'Content-Type': 'text/html; charset=utf-8',
+    });
   }
 
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build Fast. Scale Big. All in One Place.',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
+  try {
+    throwIfMissing(req.query, ['From', 'To', 'Body']);
+  } catch (err) {
+    return res.json({ ok: false, error: err.message }, 400);
+  }
+
+  const from = req.query.From;
+  const to = req.query.To;
+  const body = req.query.Body || 'Ahoy, World!';
+
+  twilioClient = twilio(accountSid, authToken);
+  try
+  {
+    const message = await twilioClient.messages.create({ body, to, from });
+    return res.json({ message: message.sid });
+}catch(error){
+        return res.json({ error: error });
+    }
 };
